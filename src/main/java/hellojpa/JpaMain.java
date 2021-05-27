6d427f1a84b4dea9069c1c18b849ee887e394afa
smpark1020 <smpark911020@gmail.com>
2021-05-27 23:50:27 +0900

    양방향 연관관계와 연관관계의 주인 2 - 주의점, 정리(ing)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -23,19 +23,20 @@ public class JpaMain {
 
             Member member = new Member();
             member.setUsername("member1");
-            member.setTeam(team);
             em.persist(member);
+
+            team.addMember(member);
 
             em.flush();
             em.clear();
 
-            Member findMember = em.find(Member.class, member.getId());
-            List<Member> members = findMember.getTeam().getMembers();
-            
-            for (Member m : members) {
-                System.out.println("m.getUsername() = " + m.getUsername());
-            }
-            
+            Team findTeam = em.find(Team.class, team.getId());
+            List<Member> members = findTeam.getMembers();
+
+            System.out.println("================");
+            System.out.println("members = " + findTeam);
+            System.out.println("================");
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
